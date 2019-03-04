f9b924e06ad3ab44b9993b94cd490be380ab5a74
David Burioni <david.burioni@libero.it>
2019-03-04 19:10:25 +0100

    add approssimate_size keyword and return explnation

--- humansize.py
+++ humansize.py
@@ -2,9 +2,15 @@ SUFFIXES = ['Kb','MB','GB','TB','PB','EB','YB']
 
 
 def approssimate_size(size):
-    if size <0:
+    """Convert a file size to human-readle form.
+    Keyword arguments:
+    size -- file size in bytes
+    
+    Returns: string
+    """ 
+    if size < 0:
         raise ValueError('number must be non-negative')
-        
+
     multiple = 1024
     for suffix in SUFFIXES:
         size /=SUFFIXES[multiple]:
